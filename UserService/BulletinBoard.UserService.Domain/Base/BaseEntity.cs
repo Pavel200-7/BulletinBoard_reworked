9c64f241d653042f655b1e55bdf43276c4376bdc
Pahan200-7 <pavlik.yakovlev.2000makron@mail.ru>
2025-11-16 18:42:04 +0300

    новый месяц - новое начало проекта.

---
+++ UserService/BulletinBoard.UserService.Domain/Base/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletinBoard.UserService.Domain.Base;
+
+/// <summary>
+/// Базовая сущность
+/// </summary>
+public class BaseEntity
+{
+    /// <summary>
+    /// Базовый идентификатор сущности.
+    /// </summary>
+    public Guid Id { get; set; }
+}
---
+++ UserService/BulletinBoard.UserService.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using BulletinBoard.UserService.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletinBoard.UserService.Domain.Entities;
+
+public class User : BaseEntity
+{
+}
